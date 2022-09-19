e1d470705a85887e0c6aebcc027d3ffdb7277d07
Taylor Guenthner <wtguenthner@gmail.com>
2022-09-19 18:30:43 -0400

    Format changes to Nav and Projects

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,12 +2,14 @@ import './Nav.css';
 
 const Nav = props =>
     <nav>
-        <h1>Taylor Guenthner</h1>
-        <div>
+        <div><h1>Taylor Guenthner</h1></div>
+        <div className='links'>
+            
             <button onClick = {() => props.page('about')}>About Me</button>
             <button onClick = {() => props.page('projects')}>Projects</button>
             <button onClick = {() => props.page('contact')}>Contact</button>
             <button onClick = {() => props.page('resume')}>Resume</button>
+            
         </div>
     </nav>
 
