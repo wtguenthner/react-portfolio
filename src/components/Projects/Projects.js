04097ff15ee4cba1c85f0035085c11bec5ce4046
Taylor Guenthner <wtguenthner@gmail.com>
2022-09-26 18:14:02 -0400

    Added additional project images

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,15 +2,14 @@ import "./Projects.css";
 import projects from "./Projects.json";
 import dungeon from './img/DungeonsNDevs.png';
 import eventFinder from './img/event-finder.png';
-import wk1 from './img/work1.jpeg'
-import wk2 from './img/work2.jpeg'
-
+import weatherApp from './img/weatherapp.png';
+import planner from './img/planner.png'
 const Projects = () =>
     <main className="Projects">
         {projects.map(app=>
             <section key={app.key} className='app'>
                
-                <a href={app.link}><img src={app.image=="dungeon"? dungeon:eventFinder} />
+                <a href={app.link}><img src={app.image=="dungeon"? dungeon:app.image =="event"?eventFinder: app.image == "weather"? weatherApp: planner} />
                 
                 {/* <div>
                     <h2>{app.title}</h2>
