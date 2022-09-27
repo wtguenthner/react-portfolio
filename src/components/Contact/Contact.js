459c5477894258dec648083739356abe34fad81f
Taylor Guenthner <wtguenthner@gmail.com>
2022-09-27 14:50:56 -0400

    Created contact form and linked to email

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,10 +1,78 @@
 import './Contact.css'
+import React, { useState } from "react";
 
-const Contact= () =>
-    <main className='contact'>
-        <img src="https://picsum.photos/200/200" />
-        <p>This is filling space.  It should be replace later on.  This is filling space.  It should be replace later on.  This is filling space.  It should be replace later on.  This is filling space.  It should be replace later on.  This is filling space.  It should be replace later on.  </p>
-        <p>This is filling space.  It should be replace later on.  This is filling space.  It should be replace later on.  This is filling space.  It should be replace later on.  This is filling space.  It should be replace later on.  This is filling space.  It should be replace later on.  </p>
+const FORM_ENDPOINT = "https://public.herotofu.com/v1/5df62dd0-3e94-11ed-a10f-d1a38bd15d37"; 
+
+const Contact = () => {
+  const [submitted, setSubmitted] = useState(false);
+  const handleSubmit = () => {
+    setTimeout(() => {
+      setSubmitted(true);
+    }, 100);
+  };
+
+  if (submitted) {
+    return (
+      <>
+      <div className="completeContainer">
+        <div className="complete">
+            <p>Form submitted.</p>
+            Thanks for your email!
+            </div>
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <main className="contact">
+       
+    <form className = "contactForm"
+      action={FORM_ENDPOINT}
+      onSubmit={handleSubmit}
+      method="POST"
+      target="_blank"
+    > <h1>Contact Taylor</h1>
+      <div>
+        <input id="name"
+          type="text"
+          placeholder="Your name"
+          name="name"
+          required
+        />
+      </div>
+      <div >
+        <input id="email"
+          type="email"
+          placeholder="Email"
+          name="email"
+          required
+        />
+      </div>
+      <div>
+        <textarea id="input"
+          placeholder="Your message"
+          name="message"
+          required
+        />
+      </div>
+      <div>
+        <button
+          type="submit"
+        >
+          Send a message
+        </button>
+      </div>
+    </form>
     </main>
+  );
+};
+// const Contact= () =>
+//     <main className='contact'>
+//         <div className="contactForm">
+//         <p>This is filling space.  It should be replace later on.  This is filling space.  It should be replace later on.  This is filling space.  It should be replace later on.  This is filling space.  It should be replace later on.  This is filling space.  It should be replace later on.  </p>
+//         <p>This is filling space.  It should be replace later on.  This is filling space.  It should be replace later on.  This is filling space.  It should be replace later on.  This is filling space.  It should be replace later on.  This is filling space.  It should be replace later on.  </p>
+//         </div>
+//     </main>
 
     export default Contact;
