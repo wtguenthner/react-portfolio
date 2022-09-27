1e0d47f33dcf208b681396bba8e1db24d35f2a85
Taylor Guenthner <wtguenthner@gmail.com>
2022-09-27 12:32:38 -0400

    Revamped the styling and added css transitions. Also added descriptions for projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,23 +1,45 @@
 import "./Projects.css";
 import projects from "./Projects.json";
-import dungeon from './img/DungeonsNDevs.png';
-import eventFinder from './img/event-finder.png';
-import weatherApp from './img/weatherapp.png';
-import planner from './img/planner.png'
-const Projects = () =>
-    <main className="Projects">
-        {projects.map(app=>
-            <section key={app.key} className='app'>
-               
-                <a href={app.link}><img src={app.image=="dungeon"? dungeon:app.image =="event"?eventFinder: app.image == "weather"? weatherApp: planner} />
-                
-                {/* <div>
-                    <h2>{app.title}</h2>
-                    <h4>Description: {app.description}</h4>
-                </div> */}
-                </a>
-            </section>
-        )}
-    </main>
+
+import { GiSwordsEmblem } from "react-icons/gi";
+import { GiPartyPopper } from "react-icons/gi";
+import { BsFillCalendarEventFill } from "react-icons/bs";
+import { TiWeatherPartlySunny } from "react-icons/ti";
+import { MdQuiz } from "react-icons/md";
+import { ImProfile } from "react-icons/im";
+const Projects = () => (
+  <main className="Projects">
+    <div className="container">
+      {projects.map((app) => (
+        <a href={app.link}>
+          <div className="app">
+            <div class="iconBox">
+              <span className="icon">
+                {app.image == "dungeon" ? (
+                  <GiSwordsEmblem />
+                ) : app.image == "event" ? (
+                  <GiPartyPopper />
+                ) : app.image == "weather" ? (
+                  <TiWeatherPartlySunny />
+                ) : app.image == "planner" ? (
+                  <BsFillCalendarEventFill />
+                ) : app.image == "quiz" ? (
+                  <MdQuiz />
+                ) : (
+                  <ImProfile />
+                )}
+              </span>
+            </div>
+
+            <div className="description">
+              <h3>{app.title}</h3>
+              <p>{app.description}</p>
+            </div>
+          </div>
+        </a>
+      ))}
+    </div>
+  </main>
+);
 
 export default Projects;
