150fa54e898ab06d9815b939c184b2edbbc885e1
Taylor Guenthner <wtguenthner@gmail.com>
2022-11-01 13:48:31 -0400

    Added software resume link

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,5 +1,6 @@
 import './Resume.css';
 import resume1 from './assets/TG Resume 2020.pdf'
+import resume2 from './assets/Software_Resume.pdf'
 import {GrDocumentPdf} from 'react-icons/gr'
 const Resume= () =>
     <main className='resume'>
@@ -7,7 +8,7 @@ const Resume= () =>
             <div className="hospitalityResume"><h1><a href= {resume1} download="Taylor Guenthner Resume"><GrDocumentPdf/></a></h1>
             <span>Hospitality Resume</span>
             </div>
-            <div className="developerResume"><h1><a href= '#'download="Taylor Guenthner Resume"><GrDocumentPdf/></a></h1>
+            <div className="developerResume"><h1><a href= {resume2} download="Taylor Guenthner Resume"><GrDocumentPdf/></a></h1>
             <span>Developer Resume</span>
         
             </div>
