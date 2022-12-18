54b8ce75e3c1cf7a49a7676800ab72db54c5e64f
Taylor Guenthner <wtguenthner@gmail.com>
2022-12-18 10:41:17 -0500

    Fixed styling changes

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -5,13 +5,14 @@ import {GrDocumentPdf} from 'react-icons/gr'
 const Resume= () =>
     <main className='resume'>
         <div className="container">
-            <div className="hospitalityResume"><h1><a href= {resume1} download="Taylor Guenthner Resume"><GrDocumentPdf/></a></h1>
+            {/* <div className="hospitalityResume"><h1><a href= {resume1} download="Taylor Guenthner Resume"><GrDocumentPdf/></a></h1>
             <span>Hospitality Resume</span>
             </div>
             <div className="developerResume"><h1><a href= {resume2} download="Taylor Guenthner Resume"><GrDocumentPdf/></a></h1>
-            <span>Developer Resume</span>
+            <span>Developer Resume</span> */}
         
-            </div>
+            {/* </div> */}
+            <p>Coming Soon</p>
         </div>
     </main>
 
